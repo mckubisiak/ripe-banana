d86321f968ba97bce6fe4cdf08420fadb7abc7f7
Kubi Kubisiak <mckubisiak@gmail.com>
2021-08-05 16:38:01 -0700

    wrote intial test for create studio

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,19 @@
-import pool from '../lib/utils/pool.js';
-import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import database from '../lib/utils/database.js';
 
-describe('demo routes', () => {
+describe('studio routes', () => {
   beforeEach(() => {
-    return setup(pool);
+    return database.sync({ force: true });
   });
+
+  it('creates a new studio', async () => {
+
+    const newStudio = await request(app)
+    .post('/api/v1/studios')
+    .send({ name: 'Ghibli', city: 'Tokyo', state: 'Kanto', country: 'Japan' });
+
+    expect(newStudio.body).toEqual({ name: 'Ghibli', city: 'Tokyo', state: 'Kanto', country: 'Japan' });
+  });
+
 });
