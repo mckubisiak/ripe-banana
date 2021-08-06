b3816e5f75e47b134217fb3b6e94c48edeede049
Elise Muellerleile <eliamue@live.com>
2021-08-06 16:57:19 -0500

    get all actors test written

--- __tests__/actors.test.js
+++ __tests__/actors.test.js
@@ -22,4 +22,38 @@ describe('actor routes', () => {
       pob: 'Beirut, Lebanon'
     });
   });
+
+  it('gets all actors', async () => {
+    const actor1 = {
+      name: 'Keanu Reeves',
+      dob: Date.UTC(1964, 9, 2),
+      pob: 'Beirut, Lebanon',
+    };
+
+    const actor2 = {
+      name: 'Bill Murray',
+      dob: Date.UTC(1950, 9, 21),
+      pob: 'Beirut, Lebanon',
+    };
+
+    await Actor.bulkCreate([actor1, actor2]);
+    return request(app)
+      .get('/api/v1/actors')
+      .then((res) => {
+        expect(res.body).toEqual([
+          {
+            id: 1,
+            name: 'Keanu Reeves',
+            dob: '1964-10-01',
+            pob: 'Beirut, Lebanon',
+          },
+          {
+            id: 2,
+            name: 'Bill Murray',
+            dob: '1950-10-20',
+            pob: 'Beirut, Lebanon',
+          },
+        ]);
+      });
+  });
 });
