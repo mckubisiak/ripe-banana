46e6ad219e287d50594fd8a101fb2ccaf7857619
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-08-06 09:29:52 -0700

    Added in the getPk test, test is fialing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -25,4 +25,17 @@ describe('studio routes', () => {
       country: 'Japan',
     });
   });
+
+  it('gets a studio by id via GET', async () => {
+    const studio = await Studio.create({
+      id: 1,
+      name: 'Ghibli',
+      city: 'Tokyo',
+      state: 'Kanto',
+      country: 'Japan',
+    });
+    const res = await request(app).get('/api/v1/studios/1');
+
+    expect(res.body).toEqual(user.toJSON());
+  });
 });
