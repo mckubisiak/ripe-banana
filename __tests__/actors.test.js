09d159b95c86cb0fffe89fa5af07c0f219c625ad
Elise Muellerleile <eliamue@live.com>
2021-08-06 17:07:05 -0500

    get one actor by id, also get all actors test now passes

--- __tests__/actors.test.js
+++ __tests__/actors.test.js
@@ -27,13 +27,13 @@ describe('actor routes', () => {
     const actor1 = {
       name: 'Keanu Reeves',
       dob: Date.UTC(1964, 9, 2),
-      pob: 'Beirut, Lebanon',
+      pob: 'Beirut, Lebanon'
     };
 
     const actor2 = {
       name: 'Bill Murray',
       dob: Date.UTC(1950, 9, 21),
-      pob: 'Beirut, Lebanon',
+      pob: 'Evanston, IL'
     };
 
     await Actor.bulkCreate([actor1, actor2]);
@@ -45,15 +45,30 @@ describe('actor routes', () => {
             id: 1,
             name: 'Keanu Reeves',
             dob: '1964-10-01',
-            pob: 'Beirut, Lebanon',
+            pob: 'Beirut, Lebanon'
           },
           {
             id: 2,
             name: 'Bill Murray',
             dob: '1950-10-20',
-            pob: 'Beirut, Lebanon',
+            pob: 'Evanston, IL'
           },
         ]);
       });
+  });
+
+  it('gets an actor by PKthunder', async () => {
+    const actor = await Actor.create({
+      id: 1,
+      name: 'Bill Murray',
+      dob: '1950-10-20',
+      pob: 'Evanston, IL'
+    });
+
+    const res = await request(app).get(`/api/v1/actors/${actor.id}`);
+
+    expect(res.body).toEqual({
+      ...actor.toJSON(),
+    });
   });
 });
